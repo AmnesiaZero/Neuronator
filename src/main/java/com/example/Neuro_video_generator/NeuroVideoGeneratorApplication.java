4f3bac5802d241bb17721c2bb5f0e318b441ebb4
Aleksandr Pavlov <sanek_pavlov03@mnail.ru>
2023-08-25 15:24:23 +0400

    Initial commit

---
+++ src/main/java/com/example/Neuro_video_generator/NeuroVideoGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Neuro_video_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroVideoGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroVideoGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Neuro_video_generator/NeuroVideoGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Neuro_video_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroVideoGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
